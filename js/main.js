94bfc25e120dcd92323deb0c8235b40f97ae1aff
Zahid Srk <69967516+zahid9946@users.noreply.github.com>
2021-08-07 22:04:58 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+//bxslider
+$(document).ready(function(){
+    $('.slider').bxSlider();
+    $('#main_menu').meanmenu({
+      meanMenuContainer: '#mobile_menu',
+      meanScreenWidth :991, 
+    });
+  });
